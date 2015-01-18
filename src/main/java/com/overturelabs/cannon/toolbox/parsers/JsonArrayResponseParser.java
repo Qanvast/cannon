36c0b388468d03f1a0a0f3a4a26137803d45ecdc
Steve Tan <i@stevetan.me>
2015-01-19 00:17:57 +0800

    Add in a JsonArrayResponseParser

---
+++ src/main/java/com/overturelabs/cannon/toolbox/parsers/JsonArrayResponseParser.java
@@ -0,0 +1,31 @@
+package com.overturelabs.cannon.toolbox.parsers;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.HttpHeaderParser;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * {@link ResponseParser} for {@link org.json.JSONArray} responses.
+ * @author Steve Tan
+ */
+public class JsonArrayResponseParser implements ResponseParser<JSONArray> {
+    @Override
+    public Response parseNetworkResponse(NetworkResponse response) {
+        try {
+            String jsonString =
+                    new String(response.data, HttpHeaderParser.parseCharset(response.headers));
+            return Response.success(new JSONArray(jsonString),
+                    HttpHeaderParser.parseCacheHeaders(response));
+        } catch (UnsupportedEncodingException e) {
+            return Response.error(new ParseError(e));
+        } catch (JSONException je) {
+            return Response.error(new ParseError(je));
+        }
+    }
+}
