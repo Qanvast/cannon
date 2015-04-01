e1a8cd74065c666cb4b3f263aaaf4146e59f4393
Derrick Lee <derrick.lee@qanvast.com>
2015-04-01 15:45:02 +0800

    Added Out Of Memory versions of DiskBasedCache and BasicNetwork. Added OutOfMemoryError extending VolleyError.

---
+++ src/main/java/com/overturelabs/cannon/toolbox/BasicNetworkOOM.java
@@ -0,0 +1,30 @@
+package com.overturelabs.cannon.toolbox;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.ByteArrayPool;
+import com.android.volley.toolbox.HttpStack;
+
+/**
+ * Created by derricklee on 4/1/15.
+ */
+public class BasicNetworkOOM extends BasicNetwork {
+    public BasicNetworkOOM(HttpStack httpStack) {
+        super(httpStack);
+    }
+
+    public BasicNetworkOOM(HttpStack httpStack, ByteArrayPool pool) {
+        super(httpStack, pool);
+    }
+
+    @Override
+    public NetworkResponse performRequest(Request<?> request) throws VolleyError {
+        try {
+            return super.performRequest(request);
+        } catch (java.lang.OutOfMemoryError error) {
+            throw new com.overturelabs.cannon.toolbox.OutOfMemoryError();
+        }
+    }
+}
---
+++ src/main/java/com/overturelabs/cannon/toolbox/OutOfMemoryError.java
@@ -0,0 +1,10 @@
+package com.overturelabs.cannon.toolbox;
+
+import com.android.volley.VolleyError;
+
+/**
+ * Created by derricklee on 4/1/15.
+ */
+public class OutOfMemoryError extends VolleyError {
+
+}
